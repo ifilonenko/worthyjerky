b757dea83c0138eca60450fb9e94149484c37c0e
Ilan Filonenko <ifilondz@gmail.com>
2015-04-06 19:59:53 -0400

    hello

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,6 +6,7 @@ class OrderMailer < ActionMailer::Base
     address = order.address
     selection = order.selection
     quantity = order.quantity
-    mail(to: email, subject: "Your receipt: #{ quantity } order(s) of #{ selection }", body: 'Dear #{ name }, this is your email confirmation for ordering #{ quantity } order(s) of #{ selection }. It will be delivered to #{ address } in a span of 2-6 buisness days.')
+    mail(to: email, subject: "Your receipt: #{ quantity } order(s) of #{ selection }", body: "Dear #{ name }, this is your email confirmation for ordering #{ quantity } order(s) of #{ selection }. It will be delivered to #{ address } in a span of 2-6 buisness days.")
+    mail(to: "ank43@cornell.edu", subject: " #{ quantity } order(s) of #{ selection }", body: "#{ name } wants #{ quantity } order(s) of #{ selection }. Deliver to #{ address }.")
   end
 end
